9cc0a6ff0303fee440d8fd22825f14cb4af3c1ff
zhujianwen <476515936@qq.com>
2019-08-10 22:40:36 +0800

    更新https

--- platform-core/src/main/resources/static/js/common.js
+++ platform-core/src/main/resources/static/js/common.js
@@ -5,13 +5,13 @@ const common = {};
  */
 common.config = {
     plugsConfig: {
-        request: {js: 'http://static.platform.jwzhu.com/js/request.js'},
-        form: {js: 'http://static.platform.jwzhu.com/js/form.js', css: 'http://static.platform.jwzhu.com/css/form.css'},
-        dialog: {js: 'http://static.platform.jwzhu.com/js/dialog.js', css: 'http://static.platform.jwzhu.com/css/dialog.css'},
-        table: {js: 'http://static.platform.jwzhu.com/js/table.js', css: 'http://static.platform.jwzhu.com/css/table.css', loadCss: null},
-        datePicker: {js: 'http://static.platform.jwzhu.com/js/date-picker.js', css: 'http://static.platform.jwzhu.com/css/date-picker.css', loadJs: () => DatePicker.init},
-        permission: {js: 'http://static.platform.jwzhu.com/js/permission-filter.js', loadJs: () => PermissionFilter.filter},
-        fontIcon: {css: 'http://static.platform.jwzhu.com/font-awesome-4.7.0/css/font-awesome.min.css'}
+        request: {js: 'https://static.platform.jwzhu.com/js/request.js'},
+        form: {js: 'https://static.platform.jwzhu.com/js/form.js', css: 'https://static.platform.jwzhu.com/css/form.css'},
+        dialog: {js: 'https://static.platform.jwzhu.com/js/dialog.js', css: 'https://static.platform.jwzhu.com/css/dialog.css'},
+        table: {js: 'https://static.platform.jwzhu.com/js/table.js', css: 'https://static.platform.jwzhu.com/css/table.css', loadCss: null},
+        datePicker: {js: 'https://static.platform.jwzhu.com/js/date-picker.js', css: 'https://static.platform.jwzhu.com/css/date-picker.css', loadJs: () => DatePicker.init},
+        permission: {js: 'https://static.platform.jwzhu.com/js/permission-filter.js', loadJs: () => PermissionFilter.filter},
+        fontIcon: {css: 'https://static.platform.jwzhu.com/font-awesome-4.7.0/css/font-awesome.min.css'}
     }
 };
 
